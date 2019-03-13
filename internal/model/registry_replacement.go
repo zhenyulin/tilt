81d9da735dfdc86d7ee0fea775fcd0eeda3d6b34
Dan Miller <dmiller@windmill.engineering>
2019-03-13 16:36:09 -0400

    model: registry replacement

--- internal/model/registry_replacement.go
+++ internal/model/registry_replacement.go
@@ -1,6 +1,32 @@
 package model
 
+import (
+	"fmt"
+
+	"github.com/docker/distribution/reference"
+)
+
 type RegistryReplacement struct {
 	Old string
 	New string
+}
+
+// Replace takes performs a RegistryReplacement and a NamedTagged and returns a new NamedTag with the domain swapped out
+func Replace(rep RegistryReplacement, name reference.NamedTagged) (reference.NamedTagged, error) {
+	if reference.Domain(name) == rep.Old {
+		path := reference.Path(name)
+
+		new, err := reference.ParseNamed(fmt.Sprintf("%s/%s", rep.New, path))
+		if err != nil {
+			return nil, err
+		}
+
+		newTagged, err := reference.WithTag(new, name.Tag())
+		if err != nil {
+			return nil, err
+		}
+
+		return newTagged, nil
+	}
+	return name, nil
 }
---
+++ internal/model/registry_replacement_test.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/windmilleng/tilt/internal/container"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testCases = []struct {
+	rep      RegistryReplacement
+	name     string
+	expected string
+}{
+	{RegistryReplacement{"gcr.io", "myreg.com"}, "gcr.io/foo/bar:deadbeef", "myreg.com/foo/bar:deadbeef"},
+	{RegistryReplacement{"other.com", "myreg.com"}, "gcr.io/foo/bar:deadbeef", "gcr.io/foo/bar:deadbeef"},
+}
+
+func TestReplace(t *testing.T) {
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("Test Case #%d", i), func(t *testing.T) {
+			name := container.MustParseNamedTagged(tc.name)
+			actual, err := Replace(tc.rep, name)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, actual.String())
+		})
+	}
+}
